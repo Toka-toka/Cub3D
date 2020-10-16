f553be3df4201b3dc1c6fd60e70d705a48058933
sedric <sedric@student.21-school.ru>
2020-10-16 13:56:52 +0300

    события движения

--- src/key_events.c
+++ src/key_events.c
@@ -8,17 +8,17 @@ int	key_pressed(int keycode, t_settings *settings)
 		exit(1);
 	}
 	if (keycode == 119 || keycode == UP)
-		settings->actions->move_forward += 1;
+		settings->actions->move_forward = 1;
 	if (keycode == S || keycode == DOWN)
-		settings->actions->move_backward += 1;
+		settings->actions->move_backward = 1;
 	if (keycode == A)
-		settings->actions->move_left += 1;
+		settings->actions->move_left = 1;
 	if (keycode == D)
-		settings->actions->move_right += 1;
+		settings->actions->move_right = 1;
 	if (keycode == LEFT)
-		settings->actions->turn_left += 1;
+		settings->actions->turn_left = 1;
 	if (keycode == RIGHT)
-		settings->actions->turn_right += 1;
+		settings->actions->turn_right = 1;
 	return(0);
 }
 
@@ -46,20 +46,16 @@ int	key_released(int keycode, t_settings *settings)
 
 int	actions_call(t_settings *settings)
 {
-	if (settings->actions->move_forward > 0)
-	{
+	if (settings->actions->move_forward == 1)
 		move_forward(settings);
-		settings->actions->move_forward++;
-	}
-	if (settings->actions->move_backward > 0)
+	if (settings->actions->move_backward == 1)
 		move_backward(settings);
-	if (settings->actions->move_left > 0)
+	if (settings->actions->move_left == 1)
 		move_left(settings);
-	if (settings->actions->move_right > 0)
+	if (settings->actions->move_right == 1)
 		move_right(settings);
-	if (settings->actions->turn_left > 0 || settings->actions->turn_right > 0)
+	if (settings->actions->turn_left == 1 || settings->actions->turn_right == 1)
 		turn(settings);
-//	printf("orientation = %f/n", settings->orientation);
 	ray_emission(settings);
 	sprite_sort(settings);
 //	mlx_do_sync(settings->win->mlx);
